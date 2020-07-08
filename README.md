27daaf3d1d436de91f0fea663f70e3cc68d00b57
Francis Eclevia <franciseclevia@Franciss-iMac.local>
2020-07-07 22:33:53 -0400

    hw

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# midtermBreakdown
+# Explain the Midterm
+
+
+### capitalizeSentences
+1. Make a new empty string that will hold our result
+2. Go through each position(a.k.a. index) in our paragraph from left to right, all the way until the end.
+3. If the character two positions to the left of the current index is a period or the current index is equal to zero...
+4. Add the current index's character to the result string uppercased. 
+5. Otherwise...
+6. Add it to the result string lowercased.
+7. Continue on to the next index until at the end of the paragraph.
+8. When complete, show(a.k.a. return) the result.
+
+
+### isValidPassword
+1. The function will check if as password is valid if it meets two specific criteria.
+2. It will first check if the password's length is less than 12 characters. If yes, then it will return an answer of false. If not, true.
+3. Next, it will check if the password contains any spaces. If yes, it will return an answer of false. 
+4. If both criteria are met with false returns, the final answer returned will be true. 
+
+
+### makeHalfSquares
+1. Make a new empty array which will hold our result. An array is like a container.
+2. The current array and its data will be evaluated left to right.
+3. The first number(which is located at the first index) will be multiplied by its value(a.k.a squared) and then divided by two.
+4. That total will be added to the new array created in Step 1. 
+5. The next number will be evaluated similarly and will continue until the last number in the array.
+6. Once complete, the new array will be returned(shown).
+
+
+### countAs
+1. This function will count how many grades in an array(container) are greater than or equal to the number 90.
+2. The count will start at zero. 
+3. When the first number(from left to right in the array) is taken, it will be evaluated if it is greater than or equal to the number 90. 
+4. If it is, the count will go from zero to one.
+5. The next number will be evaluated similarly and will continue until the last number in the array.
+6. Once the all numbers have been evaluated, the total count of numbers greater than or equal to 90 will be returned(shown).
+
+
+### deleteMiddleLetters
+1. Create an empty string which will hold our result.
+2. In order to delete the middle letter, we will need to calculate the length of the string and divide by two. A function may be used to round down numbers to the nearest integer. This calculated number is the index of the omitted character. 
+3. If other index's characters are:
+   1.  an odd index number and not the same index value of the character in Step 2 or,
+   2.  an even index number, not the same index value of the character in Step 2 nor the character located at one index to the left of the character in Step 2
+   then character will be added to the new string.
---
+++ Solutions.js
@@ -0,0 +1,219 @@
+// A note on alternate solutions:
+// You can test them by switching the names of the alternate solution function
+// with the standard solution.
+
+
+const capitalizeSentences = function(paragraph) {
+    let result = '';
+  
+    for (let i = 0; i < paragraph.length; i++) {
+      if (paragraph[i - 2] === '.' || i === 0) {
+        result = result + paragraph[i].toUpperCase();
+      } else {
+        result = result + paragraph[i].toLowerCase();
+      }
+    }
+  
+    return result;
+  }
+  
+  const isValidPassword = function(password) {
+    if (password.length < 12) {
+      return false;
+    }
+  
+    for (let i = 0; i < password.length; i++) {
+      if (password[i] === ' ') {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  // returning a raw compound boolean using .includes
+  const isValidPasswordAlt = function(password) {
+    return !password.includes(' ') && password.length >= 12;
+  }
+  
+  const makeHalfSquares = function(nums) {
+    const results = [];
+    for (let i = 0; i < nums.length; i++) {
+      results.push(nums[i] ** 2 / 2);
+    }
+  
+    return results;
+  }
+  
+  // or, using .map
+  // (We'll be covering this soon!)
+  const makeHalfSquaresAlt = function(nums) {
+    return nums.map((num) => num ** 2 / 2);
+  }
+  
+  const countAs = function(grades) {
+    let count = 0;
+    for (let i = 0; i < grades.length; i++) {
+      if (grades[i] >= 90) {
+        count = count + 1;
+      }
+    }
+  
+    return count;
+  }
+  
+  // string building version with very long boolean
+  const deleteMiddleLetters = function(str) {
+    let result = '';
+    const middle = Math.floor(str.length / 2);
+    for (let i = 0; i < str.length; i++) {
+      if (str.length % 2 === 1 && i !== middle || str.length % 2 === 0 && i !== middle && i !== middle - 1) {
+        result = result + str[i];
+      }
+    }
+  
+    return result;
+  }
+  
+  // else-if string-building version
+  // not necessary, but possibly easier to follow than the very long boolean
+  const deleteMiddleLettersAlt1 = function(str) {
+    let result = '';
+    const middle = Math.floor(str.length / 2);
+    for (let i = 0; i < str.length; i++) {
+      if (str.length % 2 === 1 && i !== middle) {
+        result = result + str[i];
+      } else if (str.length % 2 === 0 && i !== middle && i !== middle - 1) {
+        result = result + str[i];
+      }
+    }
+  
+    return result;
+  }
+  
+  // nested-if string-building version
+  // probably not... good
+  // but another way to logic it!
+  const deleteMiddleLettersAlt2 = function(str) {
+    let result = '';
+    const middle = Math.floor(str.length / 2);
+    for (let i = 0; i < str.length; i++) {
+      if (str.length % 2 === 1) {
+        if(i !== middle) {
+          result = result + str[i];
+        }
+      } else if (i !== middle && i !== middle - 1) {
+          result = result + str[i];
+      }
+    }
+  
+    return result;
+  }
+  
+  // using .slice
+  const deleteMiddleLettersAlt3 = function(str) {
+    const middle = Math.floor(str.length / 2);
+    if (str.length % 2 === 1) {
+    
+      return str.slice(0, middle) + str.slice(middle + 1);
+    }
+    
+    return str.slice(0, middle - 1) + str.slice(middle + 1);
+  }
+  
+  const lastIndexOfSpace = function(str) {
+    // looping backwards version
+    for (let i = str.length - 1; i >= 0; i--) {
+      if (str[i] === ' ') {
+        return i;
+      }
+    }
+  
+    // we made it through the whole string without encountering a space and returning the index (which would've ended this function)
+    return -1;
+  }
+  
+  // looping forwards version, updating index as we go:
+  const lastIndexOfSpaceAlt1 = function(str) {
+    // default to -1 in case we don't find one
+    let lastIndex = -1;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ' ') {
+        lastIndex = i;
+      }
+    }
+  
+    return lastIndex;
+  }
+  
+  // .lastIndexOf version
+  const lastIndexOfSpaceAlt2 = function(str) {
+    return str.lastIndexOf(' ');
+  }
+  
+  const hyphenateName = function(name) {
+    let result = '';
+    for(let i = 0; i < name.length; i++) {
+      if (i === name.lastIndexOf(' ')) {
+        result = result + '-';
+      } else {
+        result = result + name[i];
+      }
+    }
+  
+    return result;
+  }
+  
+  // alternate if check using our solution to lastIndexOfSpace:
+  const hyphenateNameAlt = function(name) {
+    let result = '';
+    for(let i = 0; i < name.length; i++) {
+      if (i === lastIndexOfSpace(name)) {
+        result = result + '-';
+      } else {
+        result = result + name[i];
+      }
+    }
+  
+    return result;
+  }
+  
+  
+  if (typeof capitalizeSentences === 'undefined') {
+    capitalizeSentences = undefined;
+  }
+  
+  if (typeof isValidPassword === 'undefined') {
+    isValidPassword = undefined;
+  }
+  
+  if (typeof makeHalfSquares === 'undefined') {
+    makeHalfSquares = undefined;
+  }
+  
+  if (typeof countAs === 'undefined') {
+    countAs = undefined;
+  }
+  
+  if (typeof deleteMiddleLetters === 'undefined') {
+    deleteMiddleLetters = undefined;
+  }
+  
+  if (typeof lastIndexOfSpace === 'undefined') {
+    lastIndexOfSpace = undefined;
+  }
+  
+  if (typeof hyphenateName === 'undefined') {
+    hyphenateName = undefined;
+  }
+  
+  
+  module.exports = {
+    capitalizeSentences,
+    isValidPassword,
+    makeHalfSquares,
+    countAs,
+    deleteMiddleLetters,
+    lastIndexOfSpace,
+    hyphenateName,
+  }
